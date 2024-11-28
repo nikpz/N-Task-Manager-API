d1a4937d5b33f60907ae6dabece6ee73f103a27d
nikpz <93861978+nikpz@users.noreply.github.com>
2024-11-29 03:27:21 +0330

    Add files via upload

---
+++ src/themes/dark.js
@@ -0,0 +1,10 @@
+import { extend } from "./utils";
+import base from "./base";
+
+export default extend(base, {
+    backgroundPrimary: '#444444',
+    backgroundSecondary: '#7b7b7b',
+    textPrimary: '#fff',
+});
+
+
---
+++ src/themes/index.js
@@ -0,0 +1,7 @@
+import base from './base';
+import { mapTheme } from './utils';
+
+/** The default theme to load */
+export const DEFAULT_THEME = 'base';
+
+export const themes = { base };
